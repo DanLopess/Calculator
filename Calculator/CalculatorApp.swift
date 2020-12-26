c227a4e6aaa1dc04d0ad22cb25526cdef846a590
Daniel <36997776+DanLopess@users.noreply.github.com>
2020-12-26 16:59:30 +0000

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Daniel Lopes on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
